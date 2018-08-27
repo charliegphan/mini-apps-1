970452bccd6fcabb8a134d66b162114dc137c907
Charlie Phan <charliegphan@gmail.com>
2018-08-27 12:30:42 -0700

    implement marking cells with X or O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,45 @@
-console.log('loaded in');
+class TicTacToeGame {
+  constructor() {
+    this.currentPlayer = 'X';
+    this.turns = 0;
+    this.plays = [];
+  }
 
+  init() {
+    let cells = document.getElementsByClassName("cell");
+
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].addEventListener('click', (e) => {
+        this.markCell(i);
+      })
+    }
+  }
+
+  markCell(spot) {
+    let cell = document.getElementsByClassName(spot.toString())[0];
+    if (cell.innerHTML === 'X' || cell.innerHTML === 'O') {
+      return;
+    }
+    cell.innerHTML = this.currentPlayer;
+    this.changeCurrentPlayer();
+  }
+
+  changeCurrentPlayer() {
+    if (this.currentPlayer === 'X') {
+      this.currentPlayer = 'O';
+    } else {
+      this.currentPlayer = 'X';
+    }
+  }
+
+  checkRounds() {
+    console.log(this.turns);
+  }
+
+  displayMessage() {
+
+  }
+}
+
+const myGame = new TicTacToeGame();
+myGame.init();
