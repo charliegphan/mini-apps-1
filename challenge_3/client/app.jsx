5f95f5c561b210294260e6463e7a17ede926fd62
Charlie Phan <charliegphan@gmail.com>
2018-08-29 15:03:04 -0700

    complete name input component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,5 +1,5 @@
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super(props)
     this.state = {
       currentPage: 'home'
@@ -11,37 +11,91 @@ class App extends React.Component {
       <div>
         <button>Checkout</button>
         <InfoForm />
-        <ShippingForm />
-        <BillingForm />
-        <ConfirmationPage />
       </div>
     )
   }
 }
 
-const InfoForm = () => {
-  return (
-    <div>Info form</div>
-  )
+class InfoForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    // this.handlePasswordChange = this.handlePassWordChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value }, () => {
+      console.log('name: ', this.state.name);
+    });
+
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value }, () => {
+      console.log('email: ', this.state.email);
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value }, () => {
+      console.log('password: ', this.state.password);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <label>
+              Name
+              <input value={this.state.name} onChange={this.handleNameChange}></input>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Email
+              <input value={this.state.email} onChange={this.handleEmailChange}></input>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Password
+            <input value={this.state.password} onChange={this.handlePasswordChange}></input>
+            </label>
+          </div>
+
+        </form>
+      </div>
+    )
+  }
 }
 
-const ShippingForm = () => {
-  return (
-    <div>Shipping form</div>
-  )
-}
 
-const BillingForm = () => {
-  return (
-    <div>Billing form</div>
-  )
-}
 
-const ConfirmationPage = () => {
-  return (
-    <div>ConfirmationPage</div>
-  )
-}
+
+// const ShippingForm = () => {
+//   return (<div>shipping form lol</div>)
+// }
+
+// const BillingForm = () => {
+//   return (<div>shipping form lol</div>)
+// }
+
+// const ConfirmationPage = () => {
+//   return (
+//     <div>ConfirmationPage</div>
+//   )
+// }
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
