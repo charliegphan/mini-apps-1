cf0b23c4107f431a986069102243c79eb181952b
Charlie Phan <charliegphan@gmail.com>
2018-08-29 16:48:19 -0700

    connect monogoDB to express

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,16 +1,26 @@
-let express = require('express');
-let path = require('path');
+const express = require('express');
+const path = require('path');
+const mongodb = require('mongodb').MongoClient;
+const dbuser = require('./config')
 
-let app = express();
+const app = express();
 
-app.use(express.static('./public'))
+mongodb.connect('mongodb://'
+  + dbuser.username + ':' + dbuser.password + '@ds237922.mlab.com:37922/checkout',
+  { useNewUrlParser: true }, (err, db) => {
+    if (err) {
+      throw err;
+    }
 
-app.get('/', (req, res) => {
-  res.send(path.join(__dirname, './public.index.html'))
-})
+    app.use(express.static('./public'))
 
+    app.get('/', (req, res) => {
+      res.send(path.join(__dirname, './public.index.html'))
+    })
 
+    app.listen(3000, () => {
+      console.log('listening on port 3000 ^_^');
+    })
 
-app.listen(3000, () => {
-  console.log('listening on port 3000 ^_^');
-})
+  })
+
