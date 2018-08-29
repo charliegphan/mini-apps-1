6b0b742e395ffff9e7a39bb65cb538c5dd88e72e
Charlie Phan <charliegphan@gmail.com>
2018-08-29 12:05:33 -0700

    react being served

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,47 @@
+class App extends React.Component {
+  constructor() {
+    super(props)
+    this.state = {
+      currentPage: 'home'
+    }
+  }
 
+  render() {
+    return (
+      <div>
+        <button>Checkout</button>
+        <InfoForm />
+        <ShippingForm />
+        <BillingForm />
+        <ConfirmationPage />
+      </div>
+    )
+  }
+}
+
+const InfoForm = () => {
+  return (
+    <div>Info form</div>
+  )
+}
+
+const ShippingForm = () => {
+  return (
+    <div>Shipping form</div>
+  )
+}
+
+const BillingForm = () => {
+  return (
+    <div>Billing form</div>
+  )
+}
+
+const ConfirmationPage = () => {
+  return (
+    <div>ConfirmationPage</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
