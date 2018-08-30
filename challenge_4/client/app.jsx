e804bb54f22de286af803c7dfd7cbef4aa066ae2
Charlie Phan <charliegphan@gmail.com>
2018-08-30 15:26:20 -0700

    spaces now know their spot on the board

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -6,45 +6,60 @@ class App extends React.Component {
     super(props);
     this.state = {
       board: [
-        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null], // R OW
         [null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null]
-      ]
+      ],
+      currentPlayer: 'RED'
     }
   }
 
-  componentDidMount() {
+  handlePiecePlacement(spot) {
 
   }
 
   render() {
     return (
-      <div>
+      <div className="app">
+        <h1>connect4haha</h1>
+        <h2>TURN: {this.state.currentPlayer}</h2>
+
         {this.state.board.map((row, column) => {
-          return <Row key={column} column={this.state.board[column]} />
+          return (
+            <Row
+              key={column}
+              row={column}
+              column={this.state.board[column]}
+            />
+          )
+
         })}
       </div>
     )
   }
 }
 
-const Row = ({ column }) => {
+const Row = ({ column, row }) => {
   return (
     <div className="row">
       {column.map((space, i) => {
-        return <Space key={i} />
+        return <Space key={i} spot={{ row, i }} />
       })
       }
     </div>
   )
 }
 
-const Space = () => {
-
-  return (<div className="space"></div>)
+const Space = ({ spot }) => {
+  return (
+    <div
+      className="space"
+      onClick={() => { console.log(spot) }}>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
